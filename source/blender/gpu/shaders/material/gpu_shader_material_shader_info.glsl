9729a5f415110b7a377195bd9dd406ab42f1d7c2
Lateasusual <chris@lateasusual.com>
2021-05-21 15:39:27 +0100

    Normalise lamp count in shader info shadows

--- source/blender/gpu/shaders/material/gpu_shader_material_shader_info.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_shader_info.glsl
@@ -9,6 +9,7 @@ void node_shader_info(vec3 position, vec3 normal,
     vec3 n_n = normalize(normal);
 
     float shadow_accum = 0.0;
+    int light_accum = 0;
     half_light = vec4(0.0);
 
     for (int i = 0; i < laNumLight && i < MAX_LIGHT; i++) {
@@ -18,11 +19,12 @@ void node_shader_info(vec3 position, vec3 normal,
         }
         // shadows *= light.data.l_color * (light.data.l_diff * light.vis * light.contact_shadow);
         shadow_accum += (1 - light.vis);
+        light_accum++;
 
         float radiance = light_diffuse(light.data, n_n, cl_common.V, light.L);
         half_light += vec4(light.data.l_color * light.data.l_diff * radiance, 0.0);
     }
 
-    shadows = vec4(1 - (shadow_accum / laNumLight));
+    shadows = vec4(1 - (shadow_accum / max(light_accum, 1)));
     ambient = vec4(probe_evaluate_world_diff(normal), 1.0);
 }
