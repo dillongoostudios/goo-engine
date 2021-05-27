b66afe23773f9bab4c5c084c98f1d461c842156c
Lateasusual <chris@lateasusual.com>
2021-05-27 18:30:46 +0100

    Undo custom-version version bump

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -40,13 +40,13 @@ extern "C" {
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
 
-#define BLENDER_FILE_SUBVERSION 50
+#define BLENDER_FILE_SUBVERSION 20
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and show a warning if the file
  * was written with too new a version. */
-#define BLENDER_FILE_MIN_VERSION 293
-#define BLENDER_FILE_MIN_SUBVERSION 50
+#define BLENDER_FILE_MIN_VERSION 290
+#define BLENDER_FILE_MIN_SUBVERSION 0
 
 /** User readable version string. */
 const char *BKE_blender_version_string(void);
