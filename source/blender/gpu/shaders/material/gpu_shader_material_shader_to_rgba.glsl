7ba4c487e62bedb70b069139c026bdf2125497c7
Lateasusual <chris@lateasusual.com>
2021-11-01 04:22:13 +0000

    EEVEE: Fix SSR and ShaderToRGB node incompatibility with Specular shading

--- source/blender/gpu/shaders/material/gpu_shader_material_shader_to_rgba.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_shader_to_rgba.glsl
@@ -1,18 +1,31 @@
 #ifndef VOLUMETRICS
+
+CLOSURE_EVAL_FUNCTION_DECLARE_1(node_shader_to_rgba, Glossy)
+
 void node_shader_to_rgba(Closure cl, out vec4 outcol, out float outalpha)
 {
   vec4 spec_accum = vec4(0.0);
+
+  vec4 spec_cl = vec4(0.0, 0.0, 0.0, 1.0);
+  vec4 diff_cl;
+  unpackVec4(cl.ssr_data, spec_cl, diff_cl);
+
   if (ssrToggle && FLAG_TEST(cl.flag, CLOSURE_SSR_FLAG)) {
-    vec3 V = cameraVec(worldPosition);
+    CLOSURE_VARS_DECLARE_1(Glossy);
+
     vec3 vN = normal_decode(cl.ssr_normal, viewCameraVec(viewPosition));
     vec3 N = transform_direction(ViewMatrixInverse, vN);
-    float roughness = cl.ssr_data.a;
-    float roughnessSquared = max(1e-3, roughness * roughness);
-    fallback_cubemap(N, V, worldPosition, viewPosition, roughness, roughnessSquared, spec_accum);
+
+    in_Glossy_0.N = N; /* Normalized during eval. */
+    in_Glossy_0.roughness = spec_cl.a;
+
+    CLOSURE_EVAL_FUNCTION_1(node_shader_to_rgba, Glossy);
+
+    spec_accum.rgb = out_Glossy_0.radiance;
   }
 
   outalpha = saturate(1.0 - avg(cl.transmittance));
-  outcol = vec4((spec_accum.rgb * cl.ssr_data.rgb) + cl.radiance, 1.0);
+  outcol = vec4((spec_accum.rgb * spec_cl.rgb) + cl.radiance, 1.0);
 
 #  ifdef USE_SSS
   outcol.rgb += cl.sss_irradiance.rgb * cl.sss_albedo;
