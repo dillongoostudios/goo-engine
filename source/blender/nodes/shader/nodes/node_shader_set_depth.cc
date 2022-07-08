8268009ad09ac6e7b89c85d9b4515b8325c7a597
Lateasusual <chris@lateasusual.com>
2022-07-09 00:13:37 +0100

    Fix Set Depth node compilation errors Rename BSDF -> Shader

--- source/blender/nodes/shader/nodes/node_shader_set_depth.cc
+++ source/blender/nodes/shader/nodes/node_shader_set_depth.cc
@@ -19,43 +19,41 @@
 
 #include "../node_shader_util.hh"
 
-/* **************** OUTPUT ******************** */
+namespace blender::nodes::node_shader_set_depth_cc {
 
-static bNodeSocketTemplate sh_node_set_depth_in[] = {
-    {SOCK_SHADER, N_("BSDF")},
-    {SOCK_FLOAT, N_("View Depth"), 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, PROP_NONE, SOCK_HIDE_VALUE},
-    {-1, ""},
-};
-
-static bNodeSocketTemplate sh_node_set_depth_out[] = {
-    {SOCK_SHADER, N_("BSDF")},
-    {-1, ""},
-};
-
-static int node_shader_gpu_set_depth(GPUMaterial *mat,
-                                        bNode *node,
-                                        bNodeExecData *UNUSED(execdata),
-                                        GPUNodeStack *in,
-                                        GPUNodeStack *out)
+static void node_declare(NodeDeclarationBuilder &b)
 {
-  if (!in[1].link) {
-    GPU_link(mat, "view_z_get", &in[1].link);
-  }
-
-  return GPU_stack_link(mat, node, "node_set_depth", in, out);
+    b.add_input<decl::Shader>(N_("Shader"));
+    b.add_input<decl::Float>(N_("View Depth"))
+            .hide_value(true);
+    b.add_output<decl::Shader>(N_("Shader"));
 }
+
+static int node_shader_gpu_add_shader(GPUMaterial *mat,
+                                      bNode *node,
+                                      bNodeExecData *UNUSED(execdata),
+                                      GPUNodeStack *in,
+                                      GPUNodeStack *out)
+{
+    if (!in[1].link) {
+        GPU_link(mat, "view_z_get", &in[1].link);
+    }
+
+    return GPU_stack_link(mat, node, "node_set_depth", in, out);
+}
+
+} // blender::nodes::node_shader_set_depth_cc
 
 /* node type definition */
 void register_node_type_sh_set_depth(void)
 {
+    namespace file_ns = blender::nodes::node_shader_set_depth_cc;
   static bNodeType ntype;
 
   sh_node_type_base(&ntype, SH_NODE_SET_DEPTH, "Set Depth", NODE_CLASS_SHADER);
-  node_type_socket_templates(&ntype, sh_node_set_depth_in, sh_node_set_depth_out);
-  node_type_size_preset(&ntype, NODE_SIZE_MIDDLE);
-  node_type_init(&ntype, NULL);
-  node_type_storage(&ntype, "", NULL, NULL);
-  node_type_gpu(&ntype, node_shader_gpu_set_depth);
+
+  ntype.declare = file_ns::node_declare;
+  node_type_gpu(&ntype, file_ns::node_shader_gpu_add_shader);
 
   nodeRegisterType(&ntype);
 }
