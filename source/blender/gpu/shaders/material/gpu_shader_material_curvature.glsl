8f7130c449486b328c5a9be242f4415982f661c9
Festivize <77230051+Festivize@users.noreply.github.com>
2022-11-08 15:07:23 +0800

    feat: add non-uniform scaling controls for Curvature function

--- source/blender/gpu/shaders/material/gpu_shader_material_curvature.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_curvature.glsl
@@ -1,4 +1,4 @@
 
-void node_screenspace_curvature(float iiterations, float sample_scale, float clamp_dist, out float scene_curvature, out float scene_rim) {
-    screenspace_curvature(iiterations, sample_scale, clamp_dist, scene_curvature, scene_rim);
+void node_screenspace_curvature(float iiterations, float sample_scale, float clamp_dist, float scale_x, float scale_y, out float scene_curvature, out float scene_rim) {
+    screenspace_curvature(iiterations, sample_scale, clamp_dist, scale_x, scale_y, scene_curvature, scene_rim);
 }
