a1c65e386d7ab5e05f545dfe233b2d444ec674de
Festivize <77230051+Festivize@users.noreply.github.com>
2022-11-11 16:35:11 +0800

    change Curvature input socket type to vector type

--- source/blender/nodes/shader/nodes/node_shader_curvature.cc
+++ source/blender/nodes/shader/nodes/node_shader_curvature.cc
@@ -25,8 +25,7 @@ static bNodeSocketTemplate sh_node_curvature_in[] = {
     {SOCK_FLOAT, N_("Samples"), 8.0f, 0.0f, 0.0f, 0.0f, 0.0f, 64.0f, PROP_NONE},
     {SOCK_FLOAT, N_("Sample Radius"), 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1000.0f, PROP_NONE},
     {SOCK_FLOAT, N_("Thickness"), 1.0f, 0.0f, 0.0f, 0.0f, 0.01f, 1000.0f, PROP_NONE},
-    {SOCK_FLOAT, N_("Thickness X"), 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1000.0f, PROP_NONE},
-    {SOCK_FLOAT, N_("Thickness Y"), 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1000.0f, PROP_NONE},
+    {SOCK_VECTOR, N_("Scale"), 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1000.0f, PROP_NONE},
     {-1, ""},
 };
 
